c80ebaa248c23637384a06f60dd9c8b37b0c9661
Anton Novikov <zebra@MBP-Anton.lan>
2018-07-12 16:26:59 +0300

    updates, delete some files

--- dist/js/common.js
+++
@@ -1,8 +0,0 @@
-$(function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-})
