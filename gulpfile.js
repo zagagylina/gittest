5bfe0c274092b31034b7fbfa64a2e66c65ec8c39
Anton Novikov <zebra@MBP-Anton.lan>
2018-07-27 16:38:52 +0300

    changes for gulpfile and minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,105 @@
-var gulp 		= require('gulp'),
-	sass 		= require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	concat 		= require('gulp-concat'),
-	uglify 		= require('gulp-uglifyjs'),
-	cssnano		= require('gulp-cssnano'),
-	rename		= require('gulp-rename')
-	del			= require('del'),
-	imagemin	= require('gulp-imagemin'),
-	pngquant	= require('imagemin-pngquant'),
-	cache		= require('gulp-cache'),
-	autoprefixer= require('gulp-autoprefixer');
+var gulp        = require('gulp');
+    del         = require('del');
+    connect     = require('gulp-connect');
+    sass        = require('gulp-sass');
+    plumber     = require('gulp-plumber');
+    concat      = require('gulp-concat');
+    rename      = require('gulp-rename')
+    cssnano     = require('gulp-cssnano');
 
-gulp.task('sass', function(){
-	return gulp.src('src/sass/*.scss')
-	.pipe(sass())
-	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-	.pipe(gulp.dest('dist/css/'))
-	.pipe(browserSync.reload({stream: true}))
+var autoprefixer = require('gulp-autoprefixer');
+var autoprefix = autoprefixer({ browsers: ['last 5 versions']}, { cascade: true });
+var sassOptions = {
+    plugins: [autoprefix]
+};
+
+var vendor = './bower_components';
+var destination = './dist/';
+var app = './src/';
+var path = {
+    watch: [
+        app + 'index.html',
+        app + 'js/**/*.js',
+        app + './sass/**/*.scss'
+    ],
+    // vendor:{
+    //     js:[
+    //         vendor + '/jquery/dist/jquery.min.js',
+    //         vendor + '/jqueryui/jquery-ui.min.js',
+    //         vendor + '/bootstrap/dist/js/bootstrap.min.js'
+    //     ],
+    //     css:[
+    //         vendor + '/bootstrap/dist/css/bootstrap.min.css',
+    //         vendor + '/bootstrap/dist/css/bootstrap-theme.min.css',
+    //         vendor + '/font-awesome/css/font-awesome.min.css'
+    //     ],
+    //     fonts:[
+    //         vendor + '/bootstrap/dist/fonts/**.**',
+    //         vendor + '/font-awesome/fonts/**.**'
+    //     ]
+    // }
+};
+
+//utils
+gulp.task('clean', function () {
+    del.sync([destination]);
 });
 
+//vendor
+// gulp.task('vendorjs', function () {
+//     gulp.src(path.vendor.js)
+//         .pipe(concat('vendor.js'))
+//         .pipe(gulp.dest(destination + 'vendor/js'))
+// });
+// gulp.task('vendorcss', function () {
+//     gulp.src(path.vendor.css)
+//         .pipe(concat('vendor.css'))
+//         .pipe(gulp.dest(destination + 'vendor/css'))
+// });
+// gulp.task('vendorFonts', function(){
+//     gulp.src(path.vendor.fonts)
+//         .pipe(gulp.dest(destination + 'vendor/fonts'));
+// });
+
+//front-end
 gulp.task('scripts', function(){
-	return gulp.src(['src/js/*.js'])
-	// .pipe(concat('libs.min.js'))
-	// .pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+	gulp.src(app + 'js/**/*.js')
+        .pipe(concat({ path: 'common.js'}))
+        .pipe(gulp.dest(destination + 'js'));
+});
+gulp.task('sass', function () {
+  	gulp.src(app + 'sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass(sassOptions))
+        // .pipe(concat('app.css')) // конкатенация всех сss-файлов. отключить для использования @import.
+        // .pipe(cssnano())
+        .pipe(gulp.dest(destination + 'css'));
 });
 
-// gulp.task('css-libs', ['sass'], function(){
-// 	return gulp.src('src/css/libs.css')
-// 	.pipe(cssnano())
-// 	.pipe(rename({suffix: '.min'}))
-// 	.pipe(gulp.dest('src/css'));
-// });
-
-gulp.task('browser-sync', function(){
-	browserSync({
-		server: {
-			baseDir: './'
-		},
-		notify: false
-	});
+//server
+gulp.task('reload', function () {
+  gulp.src(path.watch)
+    .pipe(connect.reload());
 });
-
-gulp.task('clean', function(){
-	return del.sync('dist');
+gulp.task('connect', function() {
+  connect.server({ root: './', livereload: true });
 });
-
-gulp.task('clear', function(){
-	return cache.clearAll();
-});
-
-// gulp.task('img', function(){
-// 	return gulp.src('src/img/**/*')
-// 	.pipe(cache(imagemin({
-// 		interlaced: true,
-// 		progressive: true,
-// 		svgoPlugins: [{removeViewBox: false}],
-// 		use: [pngquant()]
-// 	})))
-// 	.pipe(gulp.dest('dist/img'))
-// });
-
-gulp.task('watch', ['browser-sync', 'scripts'], function(){
-	gulp.watch('src/sass/*.scss', ['sass']);
-	gulp.watch('*.html', browserSync.reload);
-	gulp.watch('src/**/*.js', browserSync.reload);
-});
-
-
-gulp.task('build', ['clean', 'sass', 'scripts'], function(){
-	// var buildCss = gulp.src([
-	// 	'src/css/main.css',
-	// 	'src/css/libs.min.css'
-	// 	])
-	// .pipe(gulp.dest('dist/css'));
-
-	// var buildFonts = gulp.src('src/fonts/**/*')
-	// .pipe(gulp.dest('dist/fonts'));
-
-	// var buildJs = gulp.src('src/js/**/*')
-	// .pipe(gulp.dest('dist/js'));
-
-	// var buildHtml = gulp.src('src/*.html')
-	// .pipe(gulp.dest('dist'));
+gulp.task('watch', function () {
+  gulp.watch([app + 'index.html'], ['sass', 'reload']);
+  gulp.watch([app + 'sass/**/*.scss'], ['sass', 'reload']);
+  gulp.watch([app + 'js/**/*.js'], ['js', 'reload']);
 });
 
 
 
+gulp.task('vendor', ['vendorjs', 'vendorcss', 'vendorFonts']);
+gulp.task('app', ['scripts', 'sass']);
+gulp.task('server', ['connect', 'watch']);
 
-
-
-
-
-
-
-
-
+//default task
+gulp.task('up', [
+    'clean',
+    // 'vendor',
+    'app',
+    'server'
+]);
